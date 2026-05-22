61e2380e9bf508dab7eeca744613702d91eb16d5
Alexandr Botin <alexndr.botin@gmail.com>
2026-05-22 18:39:22 +0700

    Микрофикс

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
